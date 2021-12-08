5c5596f0cbf00bd26da9a6518bf20f073a2a2e93
jalechepito <95722836+jalechepito@users.noreply.github.com>
2021-12-07 22:52:17 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
     })
   );
 });
